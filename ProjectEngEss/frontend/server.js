d6a324ba3fbf7614fa08ab50c2e4c502899b3302
pon-ktnrp <pon.ktnrp@gmail.com>
2024-11-29 17:54:05 +0700

    edit frontend server.js

--- ProjectEngEss/frontend/server.js
+++ ProjectEngEss/frontend/server.js
@@ -1,8 +1,26 @@
-import app from './app.js';
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const PORT = 3000;
+const app = express();
 
-// Start server
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+// Define __dirname equivalent for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve static files from "public"
+app.use(express.static("public"));
+
+// Serve "HTML/index.html" for the root URL
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "html", "registerPage.html"));
+});
+
+app.get("/battlePage", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "html", "battlePage.html"));
+  });
+
+const PORT = 3001;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Frontend Server ready at http://localhost:${PORT}`);
 });
